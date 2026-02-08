a2fbdc28fd5fc73ab2a66a847655249a686cefa1
EastsCloud <easts.cloud.xmzd@outlook.com>
2026-02-08 10:39:03 -0500

    commit 1

---
+++ person.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <bits/stdc++.h>
+#include <properties.h>
+
+using namespace std;
+
+class Person{
+    Person(Properties property) {
+        property_ = property;
+    }
+
+
+
+
+private:
+    Properties property_;
+
+
+};
