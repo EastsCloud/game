f587f27da72e073938fdd82e46bc457bb124ee36
EastsCloud <easts.cloud.xmzd@outlook.com>
2026-02-08 20:20:34 -0500

    commit 2

--- location.h
+++ location.h
@@ -10,8 +10,35 @@ class Location{
         y_ = y;
     }
 
+    Location Get_next_location(char input) {
+        if(input == 'w' || input == 'W') {
+            return Location(x_-1, y_);
+        }
+        if(input == 'a' || input == 'A') {
+            return Location(x_, y_-1);
+        }
+        if(input == 's' || input == 'S') {
+            return Location(x_+1, y_);
+        }
+        if(input == 'd' || input == 'D') {
+            return Location(x_, y_+1);
+        }
+    }
 
+    int Get_x() {
+        return x_;
+    }
+
+    int Get_y() {
+        return y_;
+    }
+
+    int distance(Location a, Location b) {
+        return sqrt(abs(a.Get_x()-b.Get_x())*abs(a.Get_x()-b.Get_x()) + abs(a.Get_y()-b.Get_y())*abs(a.Get_y()-b.Get_y()));
+    }
+    
 private:
-    int 
+    int x_;
+    int y_;
 
 };
--- person.h
+++ person.h
@@ -2,19 +2,30 @@
 
 #include <bits/stdc++.h>
 #include <properties.h>
+#include <location.h>
+#include <image.h>
 
 using namespace std;
 
 class Person{
-    Person(Properties property) {
+    Person(Properties property, Location location) {
         property_ = property;
+        location_ = location;
     }
 
+    static constexpr ImageType IMAGE_TYPE = PERSON;
 
+    Location Get_location() {
+        return location_;
+    }
 
+    Location Get_next_location(char input) {
+        return location_.Get_next_location(input);
+    }
 
 private:
     Properties property_;
+    Location location_;
 
 
 };
